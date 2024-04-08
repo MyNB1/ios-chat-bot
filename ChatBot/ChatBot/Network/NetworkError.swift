d1f8a85cc13ad3fa132441720acf182d6c3e47a1
MyNB1 <dishdib61@naver.com>
2024-04-08 20:15:54 +0900

    feat: NetworkError enum 추가

---
+++ ChatBot/ChatBot/Network/NetworkError.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkError.swift
+//  ChatBot
+//
+//  Created by 둘리 on 4/8/24.
+//
+
+import Foundation
+
+enum NetworkError {
+    case requestError
+    case responseError
+    case invalidData
+    case failedRequest
+    case failedResponse
+}
