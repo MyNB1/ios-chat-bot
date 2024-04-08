9f7a8db83161577f4ff49e6a32a4a6796a535563
MyNB1 <dishdib61@naver.com>
2024-04-08 19:07:15 +0900

    feat: 네트워크 통신 테스트 코드 생성

--- ChatBot/ChatBot/Controller/ViewController.swift
+++ ChatBot/ChatBot/Controller/ViewController.swift
@@ -11,5 +11,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        do {
+            try NetworkProcess().requestNetwork(query: "String") { (response, error) in
+                if let response = response {
+                    print(response)
+                } else {
+                    print("Response nil")
+                }
+            }
+            
+        } catch {
+            print(NetworkError.failedResponse)
+        }
     }
 }
